b6696154eef9a92cc7c2f106195dc0a2c6ff111f
HereticJ <joshgcallahan@gmail.com>
2026-03-26 17:33:09 -0700

    Created initial files with barebones JS in script.

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/*
+1) Your main goal here is to have as little global code as possible. 
+Try tucking as much as you can inside factories. 
+If you only need a single instance of something (e.g. the gameboard, 
+the displayController etc.) then wrap the factory inside an IIFE 
+(module pattern) so it cannot be reused to create additional instances.
+
+2) In this project, think carefully about where each bit of logic should reside. 
+Each little piece of functionality should be able to fit in the game, 
+player or gameboard objects. Take care to put them in “logical” places. 
+Spending a little time brainstorming here can make your life much easier later!
+
+*/
+
+const gameBoardObject = {
+    gameBoard: [],
+};
+
+const player1 = {
+    name: "",
+    score: 0
+};
+
+const player2 = {
+    name: "",
+    score: 0
+};
+
+const gameFlow = {
+    player1Score: 0,
+    player2Score: 0,
+    
+};
