01353459ce1098ad3b2aa6b069e4e271232a94ae
HereticJ <joshgcallahan@gmail.com>
2026-05-11 17:31:06 -0700

    Game working in console as expected! Starting on adding UI elements.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Dictates game flow.
 function gameFlow() {
-    let turn = 0;
     // Game board represents the nine squares available in Tic-Tac-Toe ordered
     // from bottom left to upper-right.
     const GameBoard = (() => {
@@ -9,120 +8,135 @@ function gameFlow() {
     })();
 
     // Initializes two players and their chosen squares.
-    const players = {
+    let players = {
         one: "Player One",
         p1Squares: [],
         two: "Player Two",
         p2Squares: [],
     };
 
+    // Initializes player variable for functions.
+    let player;
     // Variables for players objects and board.
     const p1 = players.one;
     const p2 = players.two;
+    // Variables for player squares.
     let p1S = players.p1Squares;
     let p2S = players.p2Squares;
+    // Takes GameBoard object and copies/initializes it for new game.
     let board = GameBoard.board;
+    // Variable that breaks game loop when a win condition is met.
     let winBreak = false;
-    console.log(winBreak);
-    
-    // Loop to assign turns to players based on whether the turn is even or 
-    // odd.
-    function turnController(winBreak) {
-        let player;
-        // Checks to make sure turns don't exceed 9 turns.
-        for (turn = 0; turn <= 8; turn++) {
-            // Checks for win state in players' chosen squares array.
+    // Initializes first turn.
+    let turn = 0;
+
+    // Assigns turns to players. Even numbers for Player 1, odd for Player 2.
+    function turnController(turn, player) {
+        for (turn = 0; turn <= 9; turn++) {
+            // Checks player squares against win conditions.
             winCheck(players);
-            console.log(p1S);
-            // Breaks if a player successfully chooses three squares in a row.
+            // Breaks loop if win condition is satisfied by either player.
             if (winBreak === true) {
-                console.log(winBreak);
-                break;
-            } else {
-                // Assigns Player 1's turn as long as turn counter is an even 
-                // number.
-                if (turn == 0 || turn == 2 || turn == 4 || turn == 6 || 
-                    turn == 8) {
-                    player = players.one;
-                    return { player };
-                // Assigns Player 2's turn if turn counter is odd.
-                } else if (turn == 1 || turn == 3 || turn == 5 || turn == 7 || 
-                    turn == 9) {
-                    player = players.two;
-                    return { player };
-                } else if ((turn >= 9) && (winBreak === false)) {
-                    console.log("Oh no! It's a tie!");
-                    console.log(winBreak);
                     break;
-                }
+            // Assigns Player 1 turn when turn number is even.
+            } else if (winBreak === false && turn == 0 || turn == 2 || 
+                turn == 4 || turn == 6 || turn == 8) {
+                player === p1;
+                playerChoice(turn);
+            // Assigns Player 2 turn when turn number is odd.
+            } else if (winBreak === false && turn == 1 || turn == 3 || 
+                turn == 5 || turn == 7 || turn == 9) {
+                player === p2;
+                playerChoice(turn);
             }
         }
     };
 
-    // Assigns Player choices to their player object squares array.
-    function playerChoice(players, choice) {
-        if (winBreak === true) {
-        } else {
-            if (players === p1) {
-                p1S.push(choice);
-                console.log(`Player 1 Chose ${choice}`)
-                console.log(`Player 1 Squares: ${p1S}`)
-                turn++;
-                turnController();
-            } else {
-                p2S.push(choice);
-                console.log(`Player 2 Chose ${choice}`)
-                console.log(`Player 2 Squares: ${p2S}`)
-                turn++;
-                turnController();
-            }
+    // Assigns Player choices to their respective chosen squares array.
+    function playerChoice(turn, player, choice) {
+        if (turn == 0) {
+            choice = 1;
+            p1S.push(choice);
+            console.log(`Player 1 Chose ${choice}`)
+            console.log(`Player 1 Squares: ${p1S}`)
+        } else if (turn == 1) {
+            choice = 4;
+            p2S.push(choice);
+            console.log(`Player 2 Chose ${choice}`)
+            console.log(`Player 2 Squares: ${p2S}`)
+        } else if (turn == 2) {
+            choice = 2;
+            p1S.push(choice);
+            console.log(`Player 1 Chose ${choice}`)
+            console.log(`Player 1 Squares: ${p1S}`)
+        } else if (turn == 3) {
+            choice = 5;
+            p2S.push(choice);
+            console.log(`Player 2 Chose ${choice}`)
+            console.log(`Player 2 Squares: ${p2S}`)
+        } else if (turn == 4 && winBreak == false) {
+            choice = 7;
+            p1S.push(choice);
+            console.log(`Player 1 Chose ${choice}`)
+            console.log(`Player 1 Squares: ${p1S}`)
+        } else if (turn == 5 && winBreak == false) {
+            choice = 6;
+            p2S.push(choice);
+            console.log(`Player 2 Chose ${choice}`)
+            console.log(`Player 2 Squares: ${p2S}`)
+        } else if (turn == 6 && winBreak == false) {
+            choice = 7;
+            p1S.push(choice);
+            console.log(`Player 1 Chose ${choice}`)
+            console.log(`Player 1 Squares: ${p1S}`)
+        } else if (turn == 7 && winBreak == false) {
+            choice = 8;
+            p2S.push(choice);
+            console.log(`Player 2 Chose ${choice}`)
+            console.log(`Player 2 Squares: ${p2S}`)
+        } else if (turn == 8 && winBreak == false) {
+            choice = 9;
+            p1S.push(choice);
+            console.log(`Player 1 Chose ${choice}`)
+            console.log(`Player 1 Squares: ${p1S}`)
+        } else if (turn == 9 && winBreak == false) {
+            return console.log("Game over! It's a tie...")
         }
     };
-
-    function winCheckwww(playerSquares, requiredValues) {
-        return requiredValues.every(value => playerSquares.includes(value));
-    }
-
 
     // Checks if player squares match a win state.
     function winCheck(players) {
-        let p1Sort = p1S.sort();
-        let p2Sort = p2S.sort();
-        console.log(p1Sort.includes);
-        console.log(p2Sort);
-
-        if ((p1Sort.includes([1, 2, 3])) || (p1Sort.includes([4, 5, 6])) || 
-        (p1Sort.includes([7, 8, 9])) || (p1Sort.includes([1, 4, 7])) || 
-        (p1Sort.includes([2, 5, 8])) || (p1Sort.includes([3, 6, 9])) || 
-        (p1Sort.includes([1, 5, 7])) || (p1Sort.includes([3, 5, 7]))) {
+        if ((p1S.includes(1) && p1S.includes(2) && p1S.includes(3)) || 
+        (p1S.includes(4) && p1S.includes(5) && p1S.includes(6)) || 
+        (p1S.includes(7) && p1S.includes(8) && p1S.includes(9)) || 
+        (p1S.includes(1) && p1S.includes(4) && p1S.includes(7)) ||
+        (p1S.includes(2) && p1S.includes(5) && p1S.includes(8)) ||
+        (p1S.includes(3) && p1S.includes(6) && p1S.includes(9)) ||
+        (p1S.includes(1) && p1S.includes(5) && p1S.includes(9)) ||
+        (p1S.includes(3) && p1S.includes(5) && p1S.includes(7))) {
             winBreak = true;
-            console.log(winBreak);
-            console.log(p1S);
-            console.log("Game Over! Player 1 Wins!");
-        } else if ((p2Sort.includes([1 && 2 && 3])) || (p2Sort.includes([4 && 5 && 6])) || 
-        (p2Sort.includes([7 && 8 && 9])) || (p2Sort.includes([1 && 4 && 7])) || 
-        (p2Sort.includes([2 && 5 && 8])) || (p2Sort.includes([3 && 6 && 9])) || 
-        (p2Sort.includes([1 && 5 && 7])) || (p2Sort.includes([3 && 5 && 7]))) {
+            console.log("Game Over! Player One Wins!");
+        } else if ((p2S.includes(1) && p2S.includes(2) && p2S.includes(3)) || 
+        (p2S.includes(4) && p2S.includes(5) && p2S.includes(6)) || 
+        (p2S.includes(7) && p2S.includes(8) && p2S.includes(9)) || 
+        (p2S.includes(1) && p2S.includes(4) && p2S.includes(7)) ||
+        (p2S.includes(2) && p2S.includes(5) && p2S.includes(8)) ||
+        (p2S.includes(3) && p2S.includes(6) && p2S.includes(9)) ||
+        (p2S.includes(1) && p2S.includes(5) && p2S.includes(9)) ||
+        (p2S.includes(3) && p2S.includes(5) && p2S.includes(7))) {
             winBreak = true;
-            console.log(winBreak);
-            console.log(p2S);
-            console.log("Game Over! Player 2 Wins!");
+            console.log("Game Over! Player Two Wins!");
+        // If neither player's choices array match a win condition, ends game in a tie.
         } else if ((turn >= 9) && (winBreak === false)) {
             console.log("Game over! It's a tie...")
         }
-    };
-
-    turnController(winBreak);
-    playerChoice(p1, 2);
-    playerChoice(p2, 4);
-    playerChoice(p1, 3);
-    playerChoice(p2, 8);
-    playerChoice(p1, 5);
-    playerChoice(p2, 7);
-    playerChoice(p1, 8);
-    playerChoice(p2, 9);
-    playerChoice(p1, 1);
+    }
+    // Calls turn function to start the game.
+    turnController();
 };
+// Calls gameFlow function to start the game.
 gameFlow();
+
+
 
 
