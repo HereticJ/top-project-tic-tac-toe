8120efade3fa0311025daecdb24f106799c3a11c
HereticJ <joshgcallahan@gmail.com>
2026-06-01 17:29:48 -0700

    Having to redo a lot of this to incorporate UI elements.

--- script.js
+++ script.js
@@ -1,56 +1,205 @@
-// Dictates game flow.
-function gameFlow() {
-    // Game board represents the nine squares available in Tic-Tac-Toe ordered
-    // from bottom left to upper-right.
-    const GameBoard = (() => {
-        const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        return { board };
-    })();
+// Game board represents the nine squares available in Tic-Tac-Toe ordered
+// from bottom left to upper-right.
+const GameBoard = (() => {
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const [ele1, ele2, ele3, ele4, ele5, ele6, ele7, ele8, ele9] = board;
+    return { board };
+})()
 
-    // Initializes two players and their chosen squares.
-    let players = {
-        one: "Player One",
-        p1Squares: [],
-        two: "Player Two",
-        p2Squares: [],
+GameBoard;
+console.log(GameBoard);
+
+let tempBoard = GameBoard.board;
+console.log(tempBoard);
+
+function turnController(board) {
+    for (let i = 0; i <= board.length; i++) {
+        let turn = i;
+            if (turn % 2 === 0) {
+                p2Turn = true;
+            } else {
+                p1Turn = true;
+            }
     };
 
-    // Initializes player variable for functions.
-    let player;
-    // Variables for players objects and board.
-    const p1 = players.one;
-    const p2 = players.two;
+    function getTurn() {
+        if (p1Turn === true && p2Turn === false) {
+            return p1Turn;
+        } else if (p2Turn === true && p1Turn === false) {
+            return p2Turn;
+        }
+    };
+
+    function giveTurn() {
+        turn++    
+    };
+};
+
+turnController(tempBoard);
+const displayController = (() => {
+    const boardSquares = document.querySelector("#board");
+    const turnDisplay = document.querySelector("#turnDisplay");
+    const turnText = document.createElement("h2");
+    const squareButton = document.createElement("button");
+    turnText.textContent = "Player 1's Turn"
+        if (p1Turn === true) {
+            turnText.textContent = "Player 1's Turn"
+            turnDisplay.appendChild(turnText);
+        } else if (p1Turn === true && (gameDisplay.contains("turnDisplay") === false)) {
+            turnDisplay.textContent = "Player 1's Turn"
+            display.appendChild(turnDisplay);
+        } else if (p1Turn === false && p2Turn === true) {
+            display.removeChild(turnDisplay)
+            turnDisplay.textContent = "Player 2's Turn"
+            display.appendChild(turnDisplay);
+        }
+
+        for (let i = 0; i <= tempBoard.length; i++) {
+            boardSquares.appendChild(`${squareButton} + i`);
+        };
+    
+})();
+
+
+function createPlayer(num) {
+    const newPlayer = "p" + num;
+    let choice = 0;
+    let choices = [];
+    const getChoices = () => choices;
+    const giveChoice = (choice) => { choices.push(choice); };
+
+    return { num, newPlayer, getChoices, giveChoice };
+}
+
+const p1 = createPlayer("1");
+const p2 = createPlayer("2");
+console.log(p1);
+p1.giveChoice(1);
+console.log(p1.getChoices());
+p1.giveChoice(2);
+console.log(p1.getChoices());
+
+
+/*
+// Dictates game flow.
+function gameFlow() {
+
     // Variables for player squares.
-    let p1S = players.p1Squares;
-    let p2S = players.p2Squares;
+    let p1S = p1.squares;
+    let p2S = p2.squares;
     // Takes GameBoard object and copies/initializes it for new game.
     let board = GameBoard.board;
     // Variable that breaks game loop when a win condition is met.
     let winBreak = false;
-    // Initializes first turn.
-    let turn = 0;
+    let turn;
+    const squaresObject = document.querySelector("#squaresGrid");
 
-    // Assigns turns to players. Even numbers for Player 1, odd for Player 2.
-    function turnController(turn, player) {
-        for (turn = 0; turn <= 9; turn++) {
-            // Checks player squares against win conditions.
-            winCheck(players);
-            // Breaks loop if win condition is satisfied by either player.
-            if (winBreak === true) {
-                    break;
+    function turnController(turn) {
+        for (turn = 0; turn < 8;) {
             // Assigns Player 1 turn when turn number is even.
-            } else if (winBreak === false && turn == 0 || turn == 2 || 
-                turn == 4 || turn == 6 || turn == 8) {
-                player === p1;
-                playerChoice(turn);
+            if (turn == 0 || turn == 2 || 
+            turn == 4 || turn == 6 || turn == 8) {
+                player = p1;
+                console.log(turn);
+                return turn;
+                return player;
             // Assigns Player 2 turn when turn number is odd.
-            } else if (winBreak === false && turn == 1 || turn == 3 || 
-                turn == 5 || turn == 7 || turn == 9) {
-                player === p2;
-                playerChoice(turn);
+            } else if (turn == 1 || turn == 3 || 
+            turn == 5 || turn == 7 || turn == 9) {
+                player = p2;
+                console.log(turn);
+                return turn;
+                return player;
             }
         }
-    };
+    }
+
+    // Creates buttons to represent game board.
+    function makeBoard(GameBoard, players, turnController) {
+        const squares = document.querySelectorAll("square");
+            for (let i = 0; i < board.length; i++) {
+                var square = document.createElement("button");
+                squaresObject.appendChild(square);
+                square.value = i;
+            };
+            // Associates clicked squares with board numbers and assigns them to
+            // players.
+            square.addEventListener("click", chooseSquare);
+                function chooseSquare() {
+                    if (players == p1 && turn < 9) {
+                        p1S.push(parseInt(square.value));
+                        console.log(`Player One Squares ${p1S}`);
+                        square.textContent = "X";
+                        square.disabled;
+                        return turn++;
+                    } else if (players == p2 && turn < 9) {
+                        p2S.push(parseInt(`${square.textContent}`));
+                        console.log(`Player Two Squares ${p2S}`);
+                        square.textContent = "X";
+                        square.disabled;
+                        return turn++;
+                    }
+                }
+    }
+
+        // Assigns turns to players. Even numbers for Player 1, odd for Player 2.
+    function displayController(player, makeBoard) {
+        const turnDisplay = document.querySelector("#playerTurn");
+            // Assigns Player 1 turn when turn number is even.
+            if (turn == 0 || turn == 2 || 
+            turn == 4 || turn == 6 || turn == 8) {
+                turnDisplay.textContent = "Player 1's Turn"
+                
+            // Assigns Player 2 turn when turn number is odd.
+            } else if (turn == 1 || turn == 3 || 
+            turn == 5 || turn == 7 || turn == 9) {
+                turnDisplay.textContent = "Player 1's Turn"
+                
+            }
+    }
+
+    makeBoard()
+    displayController()
+};
+
+        
+    
+    // Checks if player squares match a win state.
+    function winCheck(players) {
+        if ((p1S.includes(1) && p1S.includes(2) && p1S.includes(3)) || 
+        (p1S.includes(4) && p1S.includes(5) && p1S.includes(6)) || 
+        (p1S.includes(7) && p1S.includes(8) && p1S.includes(9)) || 
+        (p1S.includes(1) && p1S.includes(4) && p1S.includes(7)) ||
+        (p1S.includes(2) && p1S.includes(5) && p1S.includes(8)) ||
+        (p1S.includes(3) && p1S.includes(6) && p1S.includes(9)) ||
+        (p1S.includes(1) && p1S.includes(5) && p1S.includes(9)) ||
+        (p1S.includes(3) && p1S.includes(5) && p1S.includes(7))) {
+            winBreak = true;
+            console.log("Game Over! Player One Wins!");
+        } else if ((p2S.includes(1) && p2S.includes(2) && p2S.includes(3)) || 
+        (p2S.includes(4) && p2S.includes(5) && p2S.includes(6)) || 
+        (p2S.includes(7) && p2S.includes(8) && p2S.includes(9)) || 
+        (p2S.includes(1) && p2S.includes(4) && p2S.includes(7)) ||
+        (p2S.includes(2) && p2S.includes(5) && p2S.includes(8)) ||
+        (p2S.includes(3) && p2S.includes(6) && p2S.includes(9)) ||
+        (p2S.includes(1) && p2S.includes(5) && p2S.includes(9)) ||
+        (p2S.includes(3) && p2S.includes(5) && p2S.includes(7))) {
+            winBreak = true;
+            console.log("Game Over! Player Two Wins!");
+
+        // If neither player's choices array match a win condition, ends game in a tie.
+        } else if ((turn >= 9) && (winBreak === false)) {
+            console.log("Game over! It's a tie...")
+        }
+            
+// Calls gameFlow function to start the game.
+gameFlow();
+/*
+            // Breaks loop if win condition is satisfied by either player.
+            if (winBreak === true) {
+
+            }
+
 
     // Assigns Player choices to their respective chosen squares array.
     function playerChoice(turn, player, choice) {
@@ -112,77 +261,4 @@ function gameFlow() {
             return console.log("Game over! It's a tie...")
         }
     };
-
-    // Checks if player squares match a win state.
-    function winCheck(players) {
-        if ((p1S.includes(1) && p1S.includes(2) && p1S.includes(3)) || 
-        (p1S.includes(4) && p1S.includes(5) && p1S.includes(6)) || 
-        (p1S.includes(7) && p1S.includes(8) && p1S.includes(9)) || 
-        (p1S.includes(1) && p1S.includes(4) && p1S.includes(7)) ||
-        (p1S.includes(2) && p1S.includes(5) && p1S.includes(8)) ||
-        (p1S.includes(3) && p1S.includes(6) && p1S.includes(9)) ||
-        (p1S.includes(1) && p1S.includes(5) && p1S.includes(9)) ||
-        (p1S.includes(3) && p1S.includes(5) && p1S.includes(7))) {
-            winBreak = true;
-            console.log("Game Over! Player One Wins!");
-        } else if ((p2S.includes(1) && p2S.includes(2) && p2S.includes(3)) || 
-        (p2S.includes(4) && p2S.includes(5) && p2S.includes(6)) || 
-        (p2S.includes(7) && p2S.includes(8) && p2S.includes(9)) || 
-        (p2S.includes(1) && p2S.includes(4) && p2S.includes(7)) ||
-        (p2S.includes(2) && p2S.includes(5) && p2S.includes(8)) ||
-        (p2S.includes(3) && p2S.includes(6) && p2S.includes(9)) ||
-        (p2S.includes(1) && p2S.includes(5) && p2S.includes(9)) ||
-        (p2S.includes(3) && p2S.includes(5) && p2S.includes(7))) {
-            winBreak = true;
-            console.log("Game Over! Player Two Wins!");
-        // If neither player's choices array match a win condition, ends game in a tie.
-        } else if ((turn >= 9) && (winBreak === false)) {
-            console.log("Game over! It's a tie...")
-        }
-    }
-
-    function gameUI(GameBoard, players, turnController) {
-        const squares = document.querySelector("#squaresGrid");
-        const turnPrint = document.querySelector("#playerTurn");
-        const gameDisplay = document.querySelector("#gameUI");
-        turnPrint.textContent  = `Player ${turn + 1}'s turn`;
-
-            if (turn == 0 || turn == 2 || 
-                turn == 4 || turn == 6 || turn == 8) {
-            turnPrint.textContent = "Player 1's turn"
-            } else {
-                turnPrint.textContent = "Player 2's turn"
-            };
-
-            for (let i = 0; i <= 8; i++) {
-            let square = document.createElement("button");
-            square.textContent = i;
-            squares.appendChild(square);
-
-            square.addEventListener("click", chooseSquare)
-            function chooseSquare() {
-                if (turn == 0 || turn == 2 || 
-                turn == 4 || turn == 6 || turn == 8) {
-                    square.textContent = "X";
-                    square.disabled;
-                    return turn++;
-                } else if (turn == 1 || turn == 3 || 
-                turn == 5 || turn == 7 || turn == 9) {
-                    square.textContent = "O"
-                    square.disabled;
-                    return turn++;
-                }
-            }
-        }
-    }
-        // Calls turn function to start the game.
-        turnController();
-        gameUI();
-};
-
-// Calls gameFlow function to start the game.
-gameFlow();
-
-
-
-
+    */
