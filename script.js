b870f4c17844e76098f70e0f97058d1782c3212c
HereticJ <joshgcallahan@gmail.com>
2026-06-12 17:32:19 -0700

    Added win conditions function (not yet working), added comments.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Game board represents the nine squares available in Tic-Tac-Toe ordered
-// from top left to bottom right.
+// from top left to bottom right, from 1-9.
 const GameBoard = (() => {
     const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     return { board };
 })();
 
+// Creates 9 squares and displays who's turn it is (Player 1 or 2).
 const displayController = (() => {
     const display = document.querySelector("#display");
     const turnDisplay = document.createElement("h3");
@@ -25,10 +26,13 @@ const displayController = (() => {
     };
 })();
 
+// Initializes first turn and square variable to represent player's choice.
+const allButtons = document.querySelector("#buttonWrapper");
 let turn = 1;
 let playerTurn;
 let square;
 
+// Creates player and assigns them an array to store their chosen squares.
 function createPlayer(num) {
     const newPlayer = "player" + num;
 
@@ -39,6 +43,7 @@ function createPlayer(num) {
     return { num, newPlayer, getSquares, giveSquare };
 };
 
+// Checks which player's turn it is and updates display.
 function turnController() {
     const turnCheck = (turn % 2 === 0);
     function getTurn(turn) {
@@ -51,67 +56,72 @@ function turnController() {
     return playerTurn, getTurn(turn);
 };
 
+// Checks player squares against possible win scenarios and declares a winner,
+// disabling buttons from being clicked. Written to accept winning choices in
+// any order (i.e. 123 or 231) (NOT YET WORKING).
+function winCheck(allButtons) {
+    const p1S = player1.getSquares();
+    console.log(p1S);
+    const p2S = player2.getSquares();
+        if ((p1S.includes(`1`) && p1S.includes(`2`) && p1S.includes(`3`)) || 
+            (p1S.includes(`4`) && p1S.includes(`5`) && p1S.includes(`6`)) || 
+            (p1S.includes(`7`) && p1S.includes(`8`) && p1S.includes(`9`)) || 
+            (p1S.includes(`1`) && p1S.includes(`4`) && p1S.includes(`7`)) ||
+            (p1S.includes(`2`) && p1S.includes(`5`) && p1S.includes(`8`)) ||
+            (p1S.includes(`3`) && p1S.includes(`6`) && p1S.includes(`9`)) ||
+            (p1S.includes(`1`) && p1S.includes(`5`) && p1S.includes(`9`)) ||
+            (p1S.includes(`3`) && p1S.includes(`5`) && p1S.includes(`7`))) {
+                turnDisplay.textContent = "Player 1 Wins!";
+                display.appendChild(turnDisplay);
+                allButtons.classList.add(`disabled`);
+                return p1S.includes;
+        } else if ((p2S.includes(`1`) && p2S.includes(`2`) && p2S.includes(`3`)) || 
+            (p2S.includes(`4`) && p2S.includes(`5`) && p2S.includes(`6`)) || 
+            (p2S.includes(`7`) && p2S.includes(`8`) && p2S.includes(`9`)) || 
+            (p2S.includes(`1`) && p2S.includes(`4`) && p2S.includes(`7`)) ||
+            (p2S.includes(`2`) && p2S.includes(`5`) && p2S.includes(`8`)) ||
+            (p2S.includes(`3`) && p2S.includes(`6`) && p2S.includes(`9`)) ||
+            (p2S.includes(`1`) && p2S.includes(`5`) && p2S.includes(`9`)) ||
+            (p2S.includes(`3`) && p2S.includes(`5`) && p2S.includes(`7`))) {
+                turnDisplay.textContent = "Player 2 Wins!";
+                display.appendChild(turnDisplay);
+                allButtons.classList.add(`disabled`);
+        } else if (turn >= 9) {
+            turnDisplay.textContent = "Game over! It's a tie..."
+        };
+};
+
+// Adds chosen squares to player choice pool and disables clicked buttons and
+// runs winCheck function (winCheck FUNCTION NOT WORKING).
 buttonWrapper.addEventListener("click", choice);
     function choice(button, squares) {
         let clickedButton = button.target;
-        console.log(clickedButton.value);
         let square = clickedButton.value;
             if (playerTurn === "Player 1") {
                 player1.giveSquare(square);
                 player1.getSquares();
                 console.log(player1.getSquares());
-            } else {
+                winCheck(player1, player2);
+            } else if (playerTurn === "Player 2") {
                 player2.giveSquare(square);
                 player2.getSquares();
                 console.log(player2.getSquares());
+                winCheck();
             }
         clickedButton.classList.add(`disabled`);
         clickedButton.disabled = true;
     };
 
+// Changes turns when a square is clicked.
 buttonWrapper.addEventListener("click", turnChange)
     function turnChange() {
         turn++;
         turnController();
-};
+    };
 
+// Creates players and initializes first turn.
 const player1 = createPlayer(1);
 const player2 = createPlayer(2);
 turnController();
 
-
 // git message: ""
-
-
-/*
-        
-    
-    // Checks if player squares match a win state.
-    function winCheck(players) {
-        if ((p1S.includes(1) && p1S.includes(2) && p1S.includes(3)) || 
-        (p1S.includes(4) && p1S.includes(5) && p1S.includes(6)) || 
-        (p1S.includes(7) && p1S.includes(8) && p1S.includes(9)) || 
-        (p1S.includes(1) && p1S.includes(4) && p1S.includes(7)) ||
-        (p1S.includes(2) && p1S.includes(5) && p1S.includes(8)) ||
-        (p1S.includes(3) && p1S.includes(6) && p1S.includes(9)) ||
-        (p1S.includes(1) && p1S.includes(5) && p1S.includes(9)) ||
-        (p1S.includes(3) && p1S.includes(5) && p1S.includes(7))) {
-            winBreak = true;
-            console.log("Game Over! Player One Wins!");
-        } else if ((p2S.includes(1) && p2S.includes(2) && p2S.includes(3)) || 
-        (p2S.includes(4) && p2S.includes(5) && p2S.includes(6)) || 
-        (p2S.includes(7) && p2S.includes(8) && p2S.includes(9)) || 
-        (p2S.includes(1) && p2S.includes(4) && p2S.includes(7)) ||
-        (p2S.includes(2) && p2S.includes(5) && p2S.includes(8)) ||
-        (p2S.includes(3) && p2S.includes(6) && p2S.includes(9)) ||
-        (p2S.includes(1) && p2S.includes(5) && p2S.includes(9)) ||
-        (p2S.includes(3) && p2S.includes(5) && p2S.includes(7))) {
-            winBreak = true;
-            console.log("Game Over! Player Two Wins!");
-
-        // If neither player's choices array match a win condition, ends game in a tie.
-        } else if ((turn >= 9) && (winBreak === false)) {
-            console.log("Game over! It's a tie...")
-        }
-
-*/
